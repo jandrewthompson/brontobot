3b9f686c407eec1d7ed199f0cc5a4735d4298ac1
Andrew <jathom5@andrew-laptop>
2012-09-12 08:34:13 -0500

    added @whatis handler

--- bot.hs
+++ bot.hs
@@ -7,15 +7,18 @@ import Text.Regex.Posix
 import Data.List
 import Data.List.Split
 import Data.Char
+import Network.HTTP
+import Network.Browser
+import Control.Exception
 
 server = "irc.freenode.org"
-port = 6667
+ircport = 6667
 -- chan = "#bitswebteam"
 chan = "#hsbot-testing"
 nick = "brontobot"
 
 main = do
-    h <- connectTo server (PortNumber (fromIntegral port))
+    h <- connectTo server (PortNumber (fromIntegral ircport))
     hSetBuffering h NoBuffering
     write h "NICK" nick
     write h "USER" (nick++" 0 * :hsbot")
@@ -38,19 +41,21 @@ listen h = forever $ do
                 then speak s
                 else eval h (clean s) 
     putStrLn s
-    where
-         forever a = a >> forever a
+  where
+    forever a = a >> forever a
 
     ping x    = "PING :" `isPrefixOf` x
     pong x    = write h "PONG" (':' : drop 6 x)
-
     isMsg x   = "PRIVMSG" `isInfixOf` x
-    toMe x    = "brontobot" `isInfixOf` last (splitOn ":" x)
-    
+    toMe x    = "brontobot" `isInfixOf` last (splitOn ":" x) || "@whatis" `isInfixOf` x
     speak x | isInfixOf "hello"    x  = privmsg h "I'm a brontobot." 
             | isInfixOf "hi"       x  = privmsg h "I'm a brontobot." 
             | isInfixOf "whats up" x  = privmsg h "Not much.  I'm a brontobot." 
             | isInfixOf "what's up"x  = privmsg h "Not a whole lot.  I'm a brontobot." 
+			| isInfixOf "@whatis " x  = 
+								do
+									txt <- wiki $ last $ splitOn "@whatis " x
+									privmsg h txt 
             | otherwise               = privmsg h $ pick x
 
 eval :: Handle -> String -> IO ()
@@ -79,3 +84,23 @@ pick :: [Char] -> [Char]
 pick (xs) = args !! (getR xs (0, (length args) - 1) )
     where
         args = ["It is certain","Without a doubt","Concentrate and ask again","My reply is no","Outlook not so good","So... it has come to this","So... it has come to this","So... it has come to this"]
+
+
+-- getit :: [Char] -> String
+getit x = 
+    do
+        rsp <- simpleHTTP (getRequest x)
+        (getResponseBody rsp)
+   
+wiki :: [Char] -> IO [Char]
+wiki x = 
+    do
+        dd <- getit $ "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=" ++ urlEncode x ++ "&rvprop=content&redirects&rvsection=0"  
+        do
+            return ( parse $ getAllTextMatches(dd =~ "''' (.*)" :: AllTextMatches [] [Char] ) )
+
+parse [] = "Hmm... brontobot is unsure."
+parse (xs) = last $ splitOn "'''" $ last xs 
+
+
+
