54cc9bf0f8c9f02dc002556a49911f9baf6f962f
Andrew <jathom5@andrew-laptop>
2012-10-04 17:05:21 -0500

    added Self Destruct

--- bot.hs
+++ bot.hs
@@ -2,6 +2,7 @@ import Network
 import System.IO
 import System.Random
 import System.Exit
+import System.Process
 import Text.Printf
 import Text.Regex
 import Text.Regex.Posix
@@ -11,12 +12,17 @@ import Data.Char
 import Network.HTTP
 import Network.Browser
 import Control.Exception
+import Web.Encodings
+import Control.Monad
+import Control.Concurrent
+import Control.Exception as E
+import Control.Concurrent.STM
 
 server = "irc.freenode.org"
 ircport = 6667
 -- chan = "#bitswebteam"
-chan = "#hsbot-testing"
-nick = "brontobot"
+chan = "#sbot-testing2"
+nick = "brontobot2"
 
 main = do
     h <- connectTo server (PortNumber (fromIntegral ircport))
@@ -24,7 +30,7 @@ main = do
     write h "NICK" nick
     write h "USER" (nick++" 0 * :hsbot")
     write h "JOIN" chan
-    listen h
+    forkIO $ listen h
 
 write :: Handle -> String -> String -> IO ()
 write h s t = do
@@ -39,7 +45,7 @@ listen h = forever $ do
         then pong s 
         else
             if isMsg s && toMe (clean s) 
-                then speak s
+                then speak s 
                 else eval h (clean s) 
     putStrLn s
   where
@@ -48,16 +54,32 @@ listen h = forever $ do
     ping x    = "PING :" `isPrefixOf` x
     pong x    = write h "PONG" (':' : drop 6 x)
     isMsg x   = "PRIVMSG" `isInfixOf` x
-    toMe x    = "brontobot" `isInfixOf` last (splitOn ":" x) || "@whatis" `isInfixOf` x
-    speak x | isInfixOf "hello"    x  = privmsg h "I'm a brontobot." 
-            | isInfixOf "hi"       x  = privmsg h "I'm a brontobot." 
-            | isInfixOf "whats up" x  = privmsg h "Not much.  I'm a brontobot." 
-            | isInfixOf "what's up"x  = privmsg h "Not a whole lot.  I'm a brontobot." 
-			| isInfixOf "@whatis " x  = 
-								do
-									txt <- wiki $ last $ splitOn "@whatis " x
-									privmsg h txt 
-            | otherwise               = privmsg h $ pick x
+    toMe x    = nick `isInfixOf` last (splitOn ":" x) || nick `isInfixOf` (last $ init (splitOn ":" x) ::[Char]) || "$" `isInfixOf` x
+    
+   
+    speak x   | isInfixOf "hello"    x  = privmsg h "I'm a brontobot." 
+              | isInfixOf "hi"       x  = privmsg h "I am brontobot, servant of the Secret Fire... wielder of the Flame of Arnor" 
+              | isInfixOf "whats up" x  = privmsg h "Not much.  I'm a brontobot." 
+              | isInfixOf "what's up"x  = privmsg h "Not a whole lot.  I'm a brontobot." 
+              | isInfixOf "self destruct"x  = do 
+                            privmsg h "Ftzzzzzzzzzz...." 
+                            threadDelay 1000000
+                            privmsg h "....zzzzzzzzzzzzzzz....." 
+                            threadDelay 2000000
+                            privmsg h "....zzzzzzzzzzzzzzzzzzzz....." 
+                            threadDelay 3000000
+                            privmsg h "....zzzzzzzzzzzzzzzzzzzzzzzzzzzzz....." 
+                            threadDelay 3500000
+                            privmsg h "plink!" 
+              | isInfixOf "$whatis " x  = 
+                        do
+                            txt <- wiki $ last $ splitOn "$whatis " x
+                            privmsg h txt 
+              | isInfixOf "$scm "x  = 
+                         do 
+                            txt <- readProcess "./commits.sh" [last $ splitOn "$scm " x] "" 
+                            privmsg h $ (last $ splitOn "$scm " x) ++ " has " ++ head (lines txt) ++ " commits"
+              | otherwise               = privmsg h $ pick x
 
 eval :: Handle -> String -> IO ()
 eval h   "!quit"                      = write h "QUIT" ":Exiting" >> exitWith ExitSuccess
@@ -84,7 +106,7 @@ getR' (_:xs) y z = getR' xs (y + 1) z
 pick :: [Char] -> [Char] 
 pick (xs) = args !! (getR xs (0, (length args) - 1) )
     where
-        args = ["It is certain","Without a doubt","Concentrate and ask again","My reply is no","Outlook not so good","So... it has come to this","So... it has come to this","So... it has come to this"]
+        args = ["I'll be right there", "Sweet fancy moses!","We'll I'll be","YOU SHALL NOT PASS!!!","I think you know","It is certain","Without a doubt","Concentrate and ask again","My reply is no","Outlook not so good","So... it has come to this","So... it has come to this","So... it has come to this"]
 
 
 -- getit :: [Char] -> String
@@ -101,7 +123,7 @@ wiki x =
             return ( parse $ getAllTextMatches(dd =~ "'''(.*)''' (.*)" :: AllTextMatches [] [Char] ) )
 
 parse [] = "Hmm... brontobot is unsure."
-parse (xs) = cleanWikiText $ last xs 
+parse (xs) = decodeHtml $ decodeHtml $ cleanWikiText $ last xs 
 
 cleanWikiText :: [Char] -> [Char]
 cleanWikiText (xs) = subRegex rx xs ""
